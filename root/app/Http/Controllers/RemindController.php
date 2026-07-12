a90b5bd6f8263cc1fccd5cc2db430189cf751d67
juichi-tomita-epkotsoftware <tomita12111@gmail.com>
2026-07-12 21:03:26 +0900

    practice: バグ注入(初心者向け・3つ目)
    リマインド一覧画面表示時に Undefined variable エラーが出る練習用バグ

--- root/app/Http/Controllers/RemindController.php
+++ root/app/Http/Controllers/RemindController.php
@@ -18,7 +18,7 @@ class RemindController extends Controller
     //一覧表示画面をかえす
     public function index()
     {
-        $reminds = $this->remindService->getList();
+        $remindList = $this->remindService->getList();
         return view('admin.reminds.indexremind', compact('reminds'));
     }
 
